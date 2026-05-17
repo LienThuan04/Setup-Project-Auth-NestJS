88ee66700f2922542fc48de730483725da5c1902
LienThuan04 <lienthuan2004@gmail.com>
2026-05-17 14:28:19 +0700

    add provider for app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { Module, ClassSerializerInterceptor } from '@nestjs/common';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { UsersModule } from '@/users/users.module';
@@ -8,24 +9,35 @@ import { RoleModule } from '@/role/role.module';
 import { SeedDbModule } from '@/seed-db/seed-db.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from '@/auth/auth.module';
-import { FilesModule } from './files/files.module';
+import { FilesModule } from '@/files/files.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { JobsModule } from '@/jobs/jobs.module';
 import { EmailModule } from '@/email/email.module';
 import { RedisModule } from '@/redis/redis.module';
+import { LoggingInterceptor } from '@/common/interceptors/logging.interceptor';
+import { TransformInterceptor } from '@/common/interceptors/transform.interceptor';
+import { AllExceptionsFilter } from '@/common/filters/all-exceptions.filter';
+import { JwtAuthGuard } from '@/lib/passport/jwt-auth.guard';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ 
+    ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ['.env.development.local', '.env.development', '.env.local', '.env'], // Load environment variables from .env files in order of priority
-     }), // Load environment variables from .env file and make them globally available
-    UsersModule, PrismaModule, SessionModule, RoleModule, 
-    SeedDbModule /*Module to seed the database with initial data on application startup*/,
-    ScheduleModule.forRoot(), /* Enable scheduling capabilities for tasks like cleaning up expired sessions */
+      envFilePath: ['.env.development.local', '.env.development', '.env.local', '.env'],
+     }),
+    UsersModule, PrismaModule, SessionModule, RoleModule,
+    SeedDbModule,
+    ScheduleModule.forRoot(),
     AuthModule, FilesModule, JobsModule, EmailModule, RedisModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
+    { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor },
+    { provide: APP_INTERCEPTOR, useClass: TransformInterceptor },
+    { provide: APP_FILTER, useClass: AllExceptionsFilter },
+    { provide: APP_GUARD, useClass: JwtAuthGuard },
+  ],
 })
 export class AppModule {}
